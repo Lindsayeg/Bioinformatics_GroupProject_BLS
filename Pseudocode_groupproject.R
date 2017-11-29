11413a169462b30b5847060207cb4be15718d187
Bhavana Palakurthi <bpalakur@nd.edu>
2017-11-29 15:59:01 +0000

    pseudocode

--- Pseudocode_groupproject.R
+++ Pseudocode_groupproject.R
@@ -1,7 +1,6 @@
 #Pseudocode to translate nucleotide sequences to aminoacid sequences
 
 #Read the aa sequence file - codonmap.txt file
-
 #Read the nucleotide fasta files control 1&2 and obsese 1&2
 #Translate nucleotide fasta files into aa fasta files
 #Reading the nucleotide fasta into aa fasta - start with startcodon and end at first stopcodon - use sed?(manual?)
