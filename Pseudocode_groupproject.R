4eae15057fa5673e1d346643df00612d527a75ae
Bhavana Palakurthi <bpalakur@nd.edu>
2017-12-06 17:24:25 +0000

    Final Pseudocode

--- Pseudocode_groupproject.R
+++ Pseudocode_groupproject.R
@@ -3,7 +3,8 @@
 #Read the aa sequence file - codonmap.txt file
 #Read the nucleotide fasta files control 1&2 and obsese 1&2
 #Translate nucleotide fasta files into aa fasta files
-#Reading the nucleotide fasta into aa fasta - start with startcodon and end at first stopcodon - using regex and translate
+#Reading the nucleotide fasta into aa fasta - start with startcodon 
+#and end at first stopcodon - using regex and translate
 
 
 ###Pseudocode for building HMM profiles
