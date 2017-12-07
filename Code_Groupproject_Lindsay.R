1463cc92201437d6051fc62cad8df0b702501104
Lindsay <lserene@nd.edu>
2017-12-07 09:33:02 -0500

    Updated code that includes protein sequences for each of the 4 original files, but lacks the correct code to do the last for loop to search translated RNAseq files for each of the 6 HMM proteins

--- Code_Groupproject_Lindsay.R
+++ Code_Groupproject_Lindsay.R
@@ -7,12 +7,13 @@
 
 #Set working directory and read each of the original neucleotide fasta files.
 setwd("~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/")
-Codon_Map <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/codonmap.txt', what = , sep="\n", character())
-Control1 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/control1.fasta', what = , sep="\n", character())
-Control2 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/control2.fasta', what = , sep="\n", character())
-Obese1 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/obese1.fasta', what = , sep="\n", character())
-Obese2 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/obese2.fasta', what = , sep="\n", character())
 
+#Read in fasta file, each line becomes an item in a vector
+Codon_Map=scan("codonmap.txt",what=character(),sep="\n")
+Control1=scan("control1.fasta",what=character(),sep="\n")
+Control2=scan("control2.fasta",what=character(),sep="\n")
+Obese1=scan("obese1.fasta",what=character(),sep="\n")
+Obese2=scan("obese2.fasta",what=character(),sep="\n")
 
 #Load package
 library('stringr')
@@ -21,10 +22,11 @@ library('devtools')
 library('seqinr')
 
 
-#Read in fasta file, each line becomes an item in a vector
-Control1=scan("control1.fasta",what=character(),sep="\n")
-
+#Create a vector one half the length of Control1, Control2, Obese1, and Obese2.
 Control1_ORF = character(length(Control1)/2)
+Control2_ORF = character(length(Control2)/2)
+Obese1_ORF = character(length(Obese2)/2)
+Obese2_ORF = character(length(Obese2)/2)
 
 ###Determine the ORF of each sequence file using a for loop.
 for (Line in 1:length(Control1)){
@@ -32,16 +34,61 @@ for (Line in 1:length(Control1)){
   #Note that str_detect returns a logical T/F
   if (!str_detect(Control1[Line],">")){
     #Find the ORF in each sequence line and write to a new file
-    #Search for (with grepexpr) the start codon (ATG)
+    #Search for the start codon (ATG) in each line of Control1 (with grepexpr) and assign it a new variable called atg_positions.
     atg_positions <- gregexpr('ATG', Control1[Line])[[1]]
+    #Determine which codons are in the first reading frame (where atg_position is divisible by 3), and assign the position of the first to a new variable called start_codon_position.
     start_codon_position <- min(atg_positions[atg_positions%%3==1])
+    #Extract sequences from each line of Control 1 from the first start codon position and place into a new variable called ORF_Temp.
     ORF_temp <- substring(Control1[Line], start_codon_position)
+    #Repeat this process for the stop codon, by first searching for each of the three stop codons in ORF_Temp.
     end_positions <- gregexpr("(TAA)|(TAG)|(TGA)", ORF_temp)[[1]]
+    #Determine which stop codons are in the first reading frame (where stop_codon_position is divisible by 3), and assign the position of the first to a new variable called end_positions.
     end_codon_position <- min(end_positions[end_positions%%3==1])
+    #Extract the sequence from each line from the start codon in ORF_temp to the first stop codon and assign it a final varible.
     Control1_ORF[Line/2] = substring(ORF_temp, 1, end_codon_position+2)
     #Control1_ORF[Line/2] = str_extract(ORF_temp,"ATG([ATGC]{3})+((TAG)|(TAA)|(TGA))?")
   }
 }
+
+
+#Repeat for each file: Control2, Obese1, and Obese2
+#Control2
+for (Line in 1:length(Control2)){
+  if (!str_detect(Control2[Line],">")){
+    atg_positions2 <- gregexpr('ATG', Control2[Line])[[1]]
+    start_codon_position2 <- min(atg_positions2[atg_positions2%%3==1])
+    ORF_temp2 <- substring(Control2[Line], start_codon_position2)
+    end_positions2 <- gregexpr("(TAA)|(TAG)|(TGA)", ORF_temp2)[[1]]
+    end_codon_position2 <- min(end_positions2[end_positions2%%3==1])
+    Control2_ORF[Line/2] = substring(ORF_temp2, 1, end_codon_position2+2)
+  }
+}
+
+#Obese1
+for (Line in 1:length(Obese1)){
+  if (!str_detect(Obese1[Line],">")){
+    atg_positions3 <- gregexpr('ATG', Obese1[Line])[[1]]
+    start_codon_position3 <- min(atg_positions3[atg_positions3%%3==1])
+    ORF_temp3 <- substring(Obese1[Line], start_codon_position3)
+    end_positions3 <- gregexpr("(TAA)|(TAG)|(TGA)", ORF_temp3)[[1]]
+    end_codon_position3 <- min(end_positions3[end_positions3%%3==1])
+    Obese1_ORF[Line/2] = substring(ORF_temp3, 1, end_codon_position3+2)
+  }
+}
+
+#Obese2
+for (Line in 1:length(Obese2)){
+  if (!str_detect(Obese2[Line],">")){
+    atg_positions4 <- gregexpr('ATG', Obese2[Line])[[1]]
+    start_codon_position4 <- min(atg_positions4[atg_positions4%%3==1])
+    ORF_temp4 <- substring(Obese2[Line], start_codon_position4)
+    end_positions4 <- gregexpr("(TAA)|(TAG)|(TGA)", ORF_temp4)[[1]]
+    end_codon_position4 <- min(end_positions4[end_positions4%%3==1])
+    Obese2_ORF[Line/2] = substring(ORF_temp4, 1, end_codon_position4+2)
+  }
+}
+
+
 
 ###Make a directory of amino acids and thier names.
 #Make a new list called amino_acid_list with the length of Codon_Map.
@@ -64,8 +111,9 @@ for (i in 1:length(amino_acid_list)){
 
   
 ### Stil need to apply amino_acid_list to the mRNA sequences to get the protein sequences.
-### Use a while loop (while i is less than or equal to the length of the reading frame, i+2)
-
+### Create a new vector the length of *_ORF and use a for loop look at each sequnce. 
+    #Set a variable to one and while that variable is less then the number of characters in each sequence of *_.ORF, 
+    #replace each set of 3 nucleotides with the corresponding amino acid. Shfit the value of the nucleotide up 3 and repeat.
 Control1_Protein = character(length(Control1_ORF))
 for (sequence in 1:length(Control1_ORF)){
   nucleotide <- 1
@@ -75,9 +123,45 @@ for (sequence in 1:length(Control1_ORF)){
   }
 }
 
+#Repeat for Control2, Obese1, and Obese2.
+#Control2
+Control2_Protein = character(length(Control2_ORF))
+for (sequence in 1:length(Control2_ORF)){
+  nucleotide <- 1
+  while (nucleotide < nchar(Control2_ORF[sequence])){
+    Control2_Protein[sequence] = paste(Control2_Protein[sequence], amino_acid_list[substr(Control2_ORF[sequence], nucleotide,nucleotide+2)], sep="")
+    nucleotide=nucleotide+3
+  }
+}
 
-#Write files out as a fasta file
-write.fasta(sequences = Control1_Protein, names = names(Control1[Line]), file.out = "Cntrl1.fasta")
+#Obese1
+Obese1_Protein = character(length(Obese1_ORF))
+for (sequence in 1:length(Obese1_ORF)){
+  nucleotide <- 1
+  while (nucleotide < nchar(Obese1_ORF[sequence])){
+    Obese1_Protein[sequence] = paste(Obese1_Protein[sequence], amino_acid_list[substr(Obese1_ORF[sequence], nucleotide,nucleotide+2)], sep="")
+    nucleotide=nucleotide+3
+  }
+}
+
+#Obese2
+Obese2_Protein = character(length(Obese2_ORF))
+for (sequence in 1:length(Obese2_ORF)){
+  nucleotide <- 1
+  while (nucleotide < nchar(Obese2_ORF[sequence])){
+    Obese2_Protein[sequence] = paste(Obese2_Protein[sequence], amino_acid_list[substr(Obese2_ORF[sequence], nucleotide,nucleotide+2)], sep="")
+    nucleotide=nucleotide+3
+  }
+}
+
+
+#Write each file out as a fasta file.
+write.fasta(sequences = as.list(Control1_Protein), names = rep("Control1", times=length(Control1_Protein)),file.out = "Control1_Protein.fasta")
+write.fasta(sequences = as.list(Control2_Protein), names = rep("Control2", times=length(Control2_Protein)),file.out = "Control2_Protein.fasta")
+write.fasta(sequences = as.list(Obese1_Protein), names = rep("Obese1", times=length(Obese1_Protein)),file.out = "Obese1_Protein.fasta")
+write.fasta(sequences = as.list(Obese2_Protein), names = rep("Obese2", times=length(Obese2_Protein)),file.out = "Obese2_Protein.fasta")
+
+
 
 
 ###This is all in UNIX on the command line.
@@ -107,7 +191,8 @@ done
 
 
 
-
+/Users/lou/Desktop/hmmer-3.1b2-macosx-intel/binaries/hmmsearch --tblout Results.txt Transcript1_Gsta2.hmm Control1_Protein.fasta
+cat Results.txt >> All_Sigma_Results.txt
 
 
 
@@ -142,3 +227,21 @@ done
 /Users/lou/Desktop/hmmer-3.1b2-macosx-intel/binaries/hmmbuild Transcript9_Lhx2.hmm Transcript9_Lhx2.align
 #Transcript 10
 /Users/lou/Desktop/hmmer-3.1b2-macosx-intel/binaries/hmmbuild Transcript10_Synpr.hmm Transcript10_Synpr.align
+
+
+
+#Create a duplicate vector of Control1 called Control1_FASTA_Export.
+Control1_FASTA_Export <- Control1
+#Use a for loop to change the even enteries of Control1_FASTA_Export to the output amino acid sequences in Control1_Protein.
+for (Line in seq(2, length(Control1_FASTA_Export), 2)){
+  Control1_FASTA_Export[Line] <- Control1_Protein[Line/2] 
+}
+
+
+#Read in files
+Codon_Map <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/codonmap.txt', what = , sep="\n", character())
+Control1 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/control1.fasta', what = , sep="\n", character())
+Control2 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/control2.fasta', what = , sep="\n", character())
+Obese1 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/obese1.fasta', what = , sep="\n", character())
+Obese2 <- scan('~/Desktop/data-shell/BioInfo_GroupProject/Bioinformatics_GroupProject_BLS/Original_Files/obese2.fasta', what = , sep="\n", character())
+
