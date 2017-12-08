691d1e6b086c7d6fa81e2383d3abf39cd0006da5
Bhavana Palakurthi <bpalakur@nd.edu>
2017-12-08 16:18:26 +0000

    Update

--- Pseudocode_groupproject.R
+++ Pseudocode_groupproject.R
@@ -1,18 +1,23 @@
 ###Pseudocode to translate nucleotide sequences to aminoacid sequences
+##Load the required packages
 #Read the aa sequence file - codonmap.txt file
 #Read the nucleotide fasta files control 1&2 and obsese 1&2
 #Translate nucleotide fasta files into aa fasta files
 #Reading the nucleotide fasta into aa fasta - start with startcodon 
-#and end at first stopcodon - using regex and translate
+#and end at first stopcodon - using regex and translate (Bhavana)
+#-using grep and while loop (Lindsay)
+#convert the translated file into fasta format
 
 
-###Pseudocode for building HMM profiles
+###Pseudocode for building HMM profiles using Unix
 #script for muscle alignment
 #forloop for muscle alignment
 #script for building HMM profile
 #for loop HMM profile Build
 #script for hmmsearch
 #for loop for hmmsearch of four RNAseq files and 6 transcript files
+#Tabulate the counts fer each protein
+
 
 
 ###Pseudocode for tophits table and identify 6 differentially expressed transcripts
@@ -22,3 +27,17 @@
 #transcript.
 #Search the NCBI protein database for amino acid sequences corresponding to
 #these 6 transcripts.
+
+##Further exploration 1
+#Use BLAST to identify the genes encoded by the 3 differentially expressed transcripts
+#Using Unix commands, make a single table that includes the top hit for each
+#transcript.
+#Search the NCBI protein database for amino acid sequences corresponding to
+#these 3 transcripts.
+
+##Further exploration 2
+#reused the hmm build script
+#identify distantly related proteins (less conserved)
+#identify the expression counts
+#tabulate the differential counts
+
